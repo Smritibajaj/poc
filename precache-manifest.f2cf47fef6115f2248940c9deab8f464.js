3a4594d5156228e46efa9848acd122d82f226f1f
simmi.bajaj <simmi.bajaj@globallogic.com>
2019-04-09 16:35:46 +0530

    Updates

---
+++ precache-manifest.f2cf47fef6115f2248940c9deab8f464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poc/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "02ec563baa99ec664be8",
+    "url": "/poc/static/js/runtime~main.b877fa7b.js"
+  },
+  {
+    "revision": "23b5f9d548f3d8ec3acf",
+    "url": "/poc/static/js/main.53346879.chunk.js"
+  },
+  {
+    "revision": "f7fcfd7d081336d1bf63",
+    "url": "/poc/static/js/2.486d5920.chunk.js"
+  },
+  {
+    "revision": "0eed9035d63d63ec3ef37688a67302ee",
+    "url": "/poc/index.html"
+  }
+];
